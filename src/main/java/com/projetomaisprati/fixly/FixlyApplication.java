04bd7fd4f4f54516e4d319a9ffc222780c286723
Fabio Morais <fabio.lmorais99@gmail.com>
2025-01-13 17:40:49 -0300

    Projeto criado

---
+++ src/main/java/com/projetomaisprati/fixly/FixlyApplication.java
@@ -0,0 +1,13 @@
+package com.projetomaisprati.fixly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FixlyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FixlyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projetomaisprati/fixly/FixlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetomaisprati.fixly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FixlyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
