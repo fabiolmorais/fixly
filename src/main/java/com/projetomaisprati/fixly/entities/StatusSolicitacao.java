8bc1f761a781635f503d3a83b4fe89216b44a527
Fabio Morais <fabio.lmorais99@gmail.com>
2025-01-13 17:53:44 -0300

    Criado os enums TipoUsuario e StatusSolicitacao e a classe Usuario

---
+++ src/main/java/com/projetomaisprati/fixly/entities/StatusSolicitacao.java
@@ -0,0 +1,5 @@
+package com.projetomaisprati.fixly.entities;
+
+public enum StatusSolicitacao {
+    PENDENTE, APROVADO, CONCLUIDO;
+}
---
+++ src/main/java/com/projetomaisprati/fixly/entities/Usuario.java
@@ -0,0 +1,76 @@
+package com.projetomaisprati.fixly.entities;
+
+import java.util.Objects;
+
+public class Usuario {
+
+    private Long id;
+    private String nome;
+    private String email;
+    private String senha;
+    private TipoUsuario tipo;
+
+    public Usuario() {
+    }
+
+    public Usuario(Long id, String nome, String email, String senha, TipoUsuario tipo) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.senha = senha;
+        this.tipo = tipo;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public TipoUsuario getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(TipoUsuario tipo) {
+        this.tipo = tipo;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Usuario usuario = (Usuario) o;
+        return Objects.equals(id, usuario.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
