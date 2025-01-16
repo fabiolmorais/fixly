478766f0b695cf6e02abc02716372b84095d6bb8
Fabio Morais <fabio.lmorais99@gmail.com>
2025-01-16 19:22:54 -0300

    Criado o endpoint GET para todos os endereços

--- src/main/java/com/projetomaisprati/fixly/controllers/EnderecoController.java
+++ src/main/java/com/projetomaisprati/fixly/controllers/EnderecoController.java
@@ -3,6 +3,8 @@ package com.projetomaisprati.fixly.controllers;
 import com.projetomaisprati.fixly.dto.EnderecoDTO;
 import com.projetomaisprati.fixly.services.EnderecoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +21,12 @@ public class EnderecoController {
     @GetMapping(value = "/{id}")
     public ResponseEntity<EnderecoDTO> findById(@PathVariable Long id) {
         EnderecoDTO dto = service.findById(id);
+        return ResponseEntity.ok(dto);
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<EnderecoDTO>> findAll (Pageable pageable) {
+        Page<EnderecoDTO> dto = service.findAll(pageable);
         return ResponseEntity.ok(dto);
     }
 }
--- src/main/java/com/projetomaisprati/fixly/services/EnderecoService.java
+++ src/main/java/com/projetomaisprati/fixly/services/EnderecoService.java
@@ -5,7 +5,10 @@ import com.projetomaisprati.fixly.entities.Endereco;
 import com.projetomaisprati.fixly.repositories.EnderecoRepository;
 import com.projetomaisprati.fixly.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class EnderecoService {
@@ -13,8 +16,15 @@ public class EnderecoService {
     @Autowired
     private EnderecoRepository repository;
 
+    @Transactional(readOnly = true)
     public EnderecoDTO findById(Long id) {
         Endereco endereco = repository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Recurso não encontrado"));
         return new EnderecoDTO(endereco);
+    }
+
+    @Transactional(readOnly = true)
+    public Page<EnderecoDTO> findAll(Pageable pageable) {
+        Page<Endereco> result = repository.findAll(pageable);
+        return result.map(x -> new EnderecoDTO(x));
     }
 }
