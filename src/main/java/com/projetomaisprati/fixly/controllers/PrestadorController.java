637b8db615d671332d2a6688030767a32c16b51e
Fabio Morais <fabio.lmorais99@gmail.com>
2025-02-04 15:25:09 -0300

    Limpado o código de prestadores

--- src/main/java/com/projetomaisprati/fixly/controllers/PrestadorController.java
+++ src/main/java/com/projetomaisprati/fixly/controllers/PrestadorController.java
@@ -1,33 +1,10 @@
 package com.projetomaisprati.fixly.controllers;
 
-import com.projetomaisprati.fixly.dto.PrestadorDTO;
-import com.projetomaisprati.fixly.dto.PrestadorDetalhadoDTO;
-import com.projetomaisprati.fixly.entities.TipoUsuario;
-import com.projetomaisprati.fixly.services.PrestadorService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/prestadores")
 public class PrestadorController {
 
-    @Autowired
-    private PrestadorService prestadorService;
-
-    @GetMapping("/buscar")
-    public ResponseEntity<Page<PrestadorDTO>> findPrestadoresByCidade(
-            @RequestParam("cidade") String cidade,
-            Pageable pageable) {
-        Page<PrestadorDTO> dto = prestadorService.findPrestadoresByCity(cidade, pageable);
-        return ResponseEntity.ok(dto);
-    }
-
-//    @GetMapping("/prestadores/{id}")
-//    public ResponseEntity<PrestadorDetalhadoDTO> findPrestadorById(@PathVariable Long id) {
-//        PrestadorDetalhadoDTO dto = prestadorService.findPrestadorById(id, TipoUsuario.PRESTADOR);
-//        return ResponseEntity.ok(dto);
-//    }
 }
--- src/main/java/com/projetomaisprati/fixly/services/PrestadorService.java
+++ src/main/java/com/projetomaisprati/fixly/services/PrestadorService.java
@@ -1,40 +1,8 @@
 package com.projetomaisprati.fixly.services;
 
-import com.projetomaisprati.fixly.dto.PrestadorDTO;
-import com.projetomaisprati.fixly.entities.Endereco;
-import com.projetomaisprati.fixly.entities.TipoUsuario;
-import com.projetomaisprati.fixly.repositories.EnderecoRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.util.Objects;
 
 @Service
 public class PrestadorService {
-
-    @Autowired
-    private EnderecoRepository enderecoRepository;
-
-    @Transactional(readOnly = true)
-    public Page<PrestadorDTO> findPrestadoresByCity(String cidade, Pageable pageable) {
-        Page<Endereco> enderecos = enderecoRepository.findByCidadeTeste(cidade, pageable);
-        return (Page<PrestadorDTO>) enderecos.map(endereco -> {
-            if (endereco.getUsuario().getTipo() == TipoUsuario.PRESTADOR) {
-                return new PrestadorDTO(endereco.getUsuario());
-            }
-            return null;
-        }).filter(Objects::nonNull);
-    }
-
-//    @Transactional(readOnly = true)
-//    public PrestadorDetalhadoDTO findPrestadorById(Long id, TipoUsuario tipoUsuario) {
-//        Usuario prestador = usuarioRepository.findPrestadorById(id, TipoUsuario.PRESTADOR)
-//                .orElseThrow(() -> new ResourceNotFoundException("Prestador não encontrado"));
-//
-//        return new PrestadorDetalhadoDTO(prestador);
-//    }
 
 }
